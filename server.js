f31f7ed6c2291a1329c568371d6d1be2d46ceca0
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2023-08-16 04:01:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRoutes);
+app.get("/", (req, res) => {
+    res.send("server is running")
+})
 let port = process.env.PORT || 5000
 mongodbConnection();
 app.listen(port, () => {
